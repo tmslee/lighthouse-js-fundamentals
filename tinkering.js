005c487bc1fb7c51df9693e8b55e265b23c33001
Sang Hyup Lee <tms.lee9@gmail.com>
2020-12-23 22:37:16 +0000

    tinkering file modified

--- tinkering.js
+++ tinkering.js
@@ -1,3 +1,30 @@
-const multiply = (a,b) => a*b;
-const result = multiply(2,4);
-console.log(result);
+const sumLargestNumbers = function(data) {
+  if(data.length === 0) return 0;
+  if(data.length === 1) return data[0];
+  if(data.length === 2) return data[0] + data[1];
+
+  let largest = Number.MIN_SAFE_INTEGER;
+  let larger = Number.MIN_SAFE_INTEGER;
+  let updated1 = false;
+  let updated2 = false;
+
+  for(let num of data){
+    if(num >= largest){
+      if(updated1 === false){
+        updated1 = true;
+      }
+      else{
+        larger = largest;
+        updated2 = true;
+      }
+      largest = num;
+    }
+    else if(num >= larger){
+      updated2 = true;
+      larger = num;
+    }
+  }
+  let sum = updated1 ? largest : 0;
+  sum = updated2 ? sum+larger : sum;
+  return sum;
+};
